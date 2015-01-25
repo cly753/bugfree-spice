806906ef4c30bdfe80c1009b79b9777c5855941b
cly753 <cly753@gmail.com>
2015-01-25 17:08:29 +0800

    clean

--- src/main/java/gen_lib.sh
+++
@@ -1,4 +0,0 @@
-path_BFLoG=/home/cly/git-workspace/bugfree-spice/try-area/the_BFLoG/BFLoG
-path_fftw3=/home/cly/git-workspace/bugfree-spice/try-area/the_BFLoG/fftw-3.3.4
-
-g++ -m64 -shared -fpic -o libbflog_api.so BFLoG_API.cpp -I$path_BFLoG -I$JAVA_HOME/include/ -I$JAVA_HOME/include/linux -I$path_fftw3/api/ -lBFLoG -lfftw3f -lm -L$path_BFLoG
